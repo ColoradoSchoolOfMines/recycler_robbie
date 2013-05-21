47154802fbb2268ffb4318244322b6792cd5c31f
Andrew DeMaria <ademaria@mines.edu>
2013-05-21 11:33:40 -0600

    Initial maven project generation

---
+++ src/main/java/edu/mines/acmX/exhibit/App.java
@@ -0,0 +1,13 @@
+package edu.mines.acmX.exhibit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
